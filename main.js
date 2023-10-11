f14f44f75462f5c5613dddcca2569dc4feedb316
Samuel Muli <samuelmuli95@gmail.com>
2023-10-11 10:53:33 +0300

    Added CSS stylesheets and linked JavaScript files to HTML

---
+++ main.js
@@ -0,0 +1,10 @@
+//So basically this is a web application that allows the strathmore leos management to keep track of their player details and manage their details.
+
+//Add a DOMContentLoaded event to the entire page
+//Fetch data from db.json to display player cards
+//Add click event to the Register New Player Button
+//Fetch data from db.json to allow POST events for registering new players
+//Add submit event to the registration form to allow registration of players
+//Add a click event to the player cards to allow pop up modal to display player details
+//Add Delete button to the player card pop up modal
+//Add a click event to the delete button that fetches data and implements the DELETE method to the db.json
